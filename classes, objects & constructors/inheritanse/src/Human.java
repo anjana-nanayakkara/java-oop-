dcc79738dd68c022a9b75a8164ed2c144d8b4b13
“anjana <74060929+anjana-nanayakkara@users.noreply.github.com>
2023-02-01 23:14:34 +0530

    constructors

---
+++ classes, objects & constructors/inheritanse/src/Human.java
@@ -0,0 +1,6 @@
+public class Human {
+    int age = 20;
+    void eat(){
+        System.out.println("He is eating.");
+    }
+}
---
+++ classes, objects & constructors/inheritanse/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // create a student object
+
+        Student std = new Student();
+        std.eat();
+        System.out.println(std.age);
+    }
+    
+}
