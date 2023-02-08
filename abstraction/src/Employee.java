00e030c3525e6b1e4b7572565923bb7fcd005cc0
“anjana <74060929+anjana-nanayakkara@users.noreply.github.com>
2023-02-08 16:33:23 +0530

    add abstraction part

---
+++ abstraction/src/Employee.java
@@ -0,0 +1,11 @@
+public class Employee extends Human{
+    int salary = 25000;
+
+    void work(){
+        System.out.println("he is working.");
+    }
+
+    void run(){
+        System.out.println("he is running");
+    }
+}
---
+++ abstraction/src/Human.java
@@ -0,0 +1,9 @@
+public abstract class Human {
+    boolean isMarried = true;
+
+    void drink(){
+        System.out.println("He is drinking.");
+    }
+
+    abstract void run();
+}
